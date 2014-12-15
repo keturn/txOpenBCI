75b114317c824af4c13265ee0d68a3554b500c65
Kevin Turner <keturn@keturn.net>
2014-12-15 15:25:11 -0800

    this is a test.

--- txopenbci/test_control.py
+++ txopenbci/test_control.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 """
 To test:
-* Protocol sends reset on connect?
 * Service sets ._client after start?
 * We send "stop streaming" on disconnect?
 * We can get the response from the "reset" command as an event with its contents
 * sensor data!
 """
+from twisted.test.proto_helpers import StringTransport
+
+from twisted.trial.unittest import TestCase
+from .control import DeviceProtocol, CMD_RESET
+
+
+class TestDeviceProtocol(TestCase):
+    def test_resetOnConnect(self):
+        protocol = DeviceProtocol()
+        transport = StringTransport()
+        protocol.makeConnection(transport)
+        self.assertEqual(CMD_RESET, transport.value())
